9790ab49d592b94cbbda47177389bf2dbe93ccbb
Tom Critchlow <tjcritchlow@gmail.com>
2020-03-05 11:42:56 -0500

    f

--- js/fastsearch.js
+++ js/fastsearch.js
@@ -134,7 +134,7 @@ function executeSearch(term) {
     searchitems = '';
   } else { // build our html 
     for (let item in results.slice(0,5)) { // only show first 5 results
-      searchitems = searchitems + '<li><a href="' + results[item].permalink + '" tabindex="0">' + '<span class="title">' + results[item].title + '</span><br /> <span class="sc">'+ results[item].section +'</span> — ' + results[item].date + ' — <em>' + results[item].desc + '</em></a></li>';
+      searchitems = searchitems + '<li><a href="' + results[item].url + '" tabindex="0">' + '<span class="title">' + results[item].title + '</span><br /> <span class="sc">'+ results[item].section +'</span> — ' + results[item].date + ' — <em>' + results[item].desc + '</em></a></li>';
     }
     resultsAvailable = true;
   }
