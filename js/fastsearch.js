63eab820e906666c5984bf01cdb0a55e162dc7e5
Tom Critchlow <tjcritchlow@gmail.com>
2020-03-15 22:47:23 -0400

    f

--- js/fastsearch.js
+++ js/fastsearch.js
@@ -159,7 +159,7 @@ function executeSearch(term) {
     resultsAvailable = false;
     searchitems = '';
   } else { // build our html 
-    for (let item in results.slice(0,5)) { // only show first 5 results
+    for (let item in results.slice(0,8)) { // only show first 5 results
       
       if(results[item].date){ //wiki pages have no date
           var date = results[item].date;
